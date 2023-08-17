0a43385e25e7e0f736f3e9fb76eeef8a93e00f8d
gerda <gerda.tumelyte@gmail.com>
2023-08-17 17:26:07 +0300

    skeleton for parsing school excel data; test almost works

--- app/Transformers/ExcelDataHandler.php
+++ app/Transformers/ExcelDataHandler.php
@@ -2,18 +2,41 @@
 
 namespace App\Transformers;
 
+use PhpParser\Node\Expr\Array_;
 use Shuchkin\SimpleXLSX;
+use Exception;
 
 class ExcelDataHandler
 {
-    public function loadSchoolDataFromExcel(string $excelFile) : array {
+    public function loadSchoolDataFromExcel(string $excelFile): array
+    {
 
-        if ($xlsx = SimpleXLSX::parse($excelFile) ) {
+        if ($xlsx = SimpleXLSX::parse($excelFile)) {
             print_r($xlsx->rows());
-            $transformedExcelData = $xlsx->rows();
-            // TODO multiple requests from xls for each sehhet
+//            $sheetCount = $xlsx->sheetsCount();
+//            for ($i = 0; $i< $sheetCount; $i++)
+//            {
+//                $sheetName = $xlsx->sheetName($i);
+//            }
+            $transformedExcelSheet = $xlsx->rows();
+            $transformedExcelSheet = array_map(function ($transformedExcelColumn)
+            {
+                return ['id' => $transformedExcelColumn[0],
+                    'dayOfWeek' => $transformedExcelColumn[1],
+                    'startTime' => $transformedExcelColumn[2],
+                    'endTime' => $transformedExcelColumn[3]];
+            }, $transformedExcelSheet
+
+            );
+
+
+            $transformedExcelSheet1 = $xlsx->rows(1);
+            $transformedExcelSheet2 = $xlsx->rows(2);
+            $transformedExcelData= array($transformedExcelSheet,$transformedExcelSheet1,$transformedExcelSheet2);
+            // TODO multiple requests from xls for each sheet
             return $transformedExcelData;
         }
-        return []; // TODO handle error
+        throw new Exception(SimpleXLSX::parseError());
+
     }
 }
