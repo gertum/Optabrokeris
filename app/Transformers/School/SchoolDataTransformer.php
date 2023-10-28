34daf3b3e0baf68e0afc2698cc24da34c5e91ae2
Giedrius Tumelis <giedrius@inkodus.lt>
2023-10-29 02:40:32 +0300

    School data transformer skeleton.

---
+++ app/Transformers/School/SchoolDataTransformer.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Transformers\School;
+
+class SchoolDataTransformer
+{
+    public function jsonToExcel(array $jsonData) : array {
+
+        // TODO
+        return [];
+    }
+
+    public function excelToJson(array $excelData) : array {
+        // TODO
+        return [];
+    }
+}
