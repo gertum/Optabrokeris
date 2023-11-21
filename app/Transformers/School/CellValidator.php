86b38efa2a8d39186ae4e757ec86ff722a1e5e87
giedrius <giedrius@inkodus.lt>
2023-11-21 13:51:21 +0200

    Skeleton for validating cells.

---
+++ app/Transformers/School/CellValidator.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Transformers\School;
+
+class CellValidator
+{
+    public static function validateCells(array $data) : bool {
+        return false;
+    }
+}
---
+++ tests/Unit/School/CellValidatorTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Tests\Unit\School;
+
+use PHPUnit\Framework\TestCase;
+
+class CellValidatorTest extends TestCase
+{
+    public function testValidate() {
+        $this->assertTrue(true);
+    }
+
+}
