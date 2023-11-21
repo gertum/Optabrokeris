e72b26e45f9bd17ec5a98b4772b354916223a88b
giedrius <giedrius@inkodus.lt>
2023-11-21 21:49:59 +0200

    Validating timeslot start date.

--- app/Transformers/School/CellValidator.php
+++ app/Transformers/School/CellValidator.php
@@ -3,6 +3,8 @@
 namespace App\Transformers\School;
 
 use App\Exceptions\ValidateException;
+use Carbon\Carbon;
+use Carbon\Exceptions\InvalidFormatException;
 
 class CellValidator
 {
@@ -18,12 +20,39 @@ class CellValidator
             }
         }
 
+        $timeslotLine = 0;
         foreach ($data['timeslotList'] as $timeslot) {
             $dayOfWeek = $timeslot['dayOfWeek'];
 
             if (!in_array($dayOfWeek, self::DAYS_OF_WEEK)) {
-                throw new ValidateException(sprintf('Invalid day of week %s, must be one of [%s]', $dayOfWeek, join(',', self::DAYS_OF_WEEK)));
+                throw new ValidateException(sprintf('Invalid day of week %s, must be one of [%s]. Line %s.',
+                    $dayOfWeek, join(',', self::DAYS_OF_WEEK), $timeslotLine));
             }
+
+            $id = $timeslot['id'];
+            if (!is_numeric($id) || intval($id) <= 0) {
+                throw new ValidateException(sprintf('Invalid timeslot id [%s]. Line %s.',
+                    $id, $timeslotLine));
+            }
+
+            $startTime = $timeslot['startTime'];
+
+            $wrongStartTime = false;
+            $details = '';
+            try {
+                $rez = Carbon::createFromFormat('H:i:s', $startTime);
+                $wrongStartTime = empty($rez);
+           } catch (InvalidFormatException $e) {
+                $details = $e->getMessage();
+                $wrongStartTime = true;
+            }
+
+            if ( $wrongStartTime ) {
+                throw new ValidateException(sprintf('Invalid timeslot start time [%s] (%s). Line %s.',
+                    $startTime, $details, $timeslotLine));
+            }
+
+            $timeslotLine++;
         }
 
         return true;
