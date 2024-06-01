77a015c848e73c7f20e8caee40de42905a4002b9
giedrius <giedrius@inkodus.lt>
2024-06-01 22:14:23 +0300

    Test for a different file and different employee.

--- tests/Unit/Roster/HospitalExcelEmployeeHourLimitTest.php
+++ tests/Unit/Roster/HospitalExcelEmployeeHourLimitTest.php
@@ -37,6 +37,12 @@ class HospitalExcelEmployeeHourLimitTest extends TestCase
                 'expectedHoursLimit' => 74,
                 'expectedName' => 'Renata Juknevičienė 29/12',
             ],
+            'test birželis 12 employee' => [
+                'file' => __DIR__ . '/data/birželis.xlsx',
+                'testedEmployeeNumber' => 11,
+                'expectedHoursLimit' => 18.40,
+                'expectedName' => 'Vesta Aleliūnienė 137/84',
+            ],
         ];
     }
 }
