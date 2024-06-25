8bd7c851e2c9d13f60aae806a292898ee57797b2
giedrius <giedrius@inkodus.lt>
2024-06-25 11:05:00 +0300

    Code comment.

--- app/Domain/Roster/Hospital/CellMatcherInterface.php
+++ app/Domain/Roster/Hospital/CellMatcherInterface.php
@@ -2,6 +2,9 @@
 
 namespace App\Domain\Roster\Hospital;
 
+/**
+ * This interface is used to generalize classes which searches cell by various marks.
+ */
 interface CellMatcherInterface
 {
     public function matchCell(Cell $cell, int $row, int $column): bool;
