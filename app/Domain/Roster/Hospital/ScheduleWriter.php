1e6c1a8ef6779ab22d8dd43d2c4aa9d80c2df04b
giedrius <giedrius@inkodus.lt>
2024-06-26 09:06:04 +0300

    Introduced alexandrainst/php-xlsx-fast-editor and tried for modifying cells.

--- app/Domain/Roster/Hospital/ScheduleWriter.php
+++ app/Domain/Roster/Hospital/ScheduleWriter.php
@@ -2,12 +2,41 @@
 
 namespace App\Domain\Roster\Hospital;
 
+use alexandrainst\XlsxFastEditor\XlsxFastEditor;
 use App\Domain\Roster\Schedule;
+use Shuchkin\SimpleXLSXGen;
 
 class ScheduleWriter
 {
-    public function writeSchedule( string $fileTemplate, Schedule $schedule, string $outputFile) {
-        // TODO
-    }
+    public function writeSchedule(string $fileTemplate, Schedule $schedule, string $outputFile)
+    {
+        $wrapper = ExcelWrapper::parse($fileTemplate);
+//        $xlsxGen = new SimpleXLSXGen();
+//        // šitas sprendimas netinka, reikia kitos bibliotekos
+//        // gal šita tiktų : alexandrainst/php-xlsx-fast-editor
+//        $sheets = $wrapper->getXlsx()->getSheets();
+//        $xlsxGen->addSheet($wrapper->getXlsx()->rows(), $sheets[0]->getName());
+//
+//        // TODO fill values from $schedule
+//
+//        $xlsxGen->setAuthor('Inkodus');
+//        $xlsxGen->setCompany('Inkodus');
+//        $xlsxGen->setManager('Inkodus');
+//        $xlsxGen->setLastModifiedBy('Inkodus');
+//
+//        $xlsxGen->saveAs($outputFile);
 
+        copy($fileTemplate, $outputFile);
+
+        // $wrapper must tell where is the cell we need to edit
+
+        $xlsxFastEditor = new XlsxFastEditor($outputFile);
+
+        $worksheetName = $xlsxFastEditor->getWorksheetName(1);
+        $worksheetId1 = $xlsxFastEditor->getWorksheetNumber($worksheetName);
+
+        $xlsxFastEditor->writeString($worksheetId1,'H10', 'test');
+
+        $xlsxFastEditor->save();
+    }
 }
