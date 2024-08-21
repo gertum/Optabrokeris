c0a65a9bb6c9246452494429402c1a005f23e72c
giedrius <giedrius@inkodus.lt>
2024-08-21 08:51:20 +0300

    Skeleton for job report transformer.

---
+++ app/Domain/Roster/Report/Employeeinfo.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\Domain\Roster\Report;
+
+class Employeeinfo
+{
+
+}
---
+++ tests/Unit/Roster/Report/AmbulanceJobReportTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Tests\Unit\Roster\Report;
+
+use Tests\TestCase;
+
+class AmbulanceJobReportTest extends TestCase
+{
+    public function testHolder()
+    {
+        // TODO EmployeeInfo, ShiftInfo
+        $this->assertTrue(true);
+    }
+}
