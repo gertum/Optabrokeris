7fa396bc55689ba1244b37d4364ba7e23ea2e3e2
Giedrius Tumelis <giedrius@inkodus.lt>
2024-11-06 13:29:04 +0200

    #20: skeleton for multiple availabilities generation test.

--- tests/Unit/Roster/CreateAvailabilitiesForMultipleDaysTest.php
+++ tests/Unit/Roster/CreateAvailabilitiesForMultipleDaysTest.php
@@ -1,12 +1,34 @@
 <?php
 
-
 namespace Tests\Unit\Roster;
 
-
+use App\Domain\Roster\Availability;
+use App\Domain\Roster\Hospital\ScheduleParser;
+use Carbon\Carbon;
 use PHPUnit\Framework\TestCase;
 
 class CreateAvailabilitiesForMultipleDaysTest extends TestCase
 {
+    /**
+     * @param string[] $values
+     * @param Availability[] $expectedAvailabilities
+     * @dataProvider provideValues
+     */
+    public function testMultiple(array $values, Carbon $startingDate, array $expectedAvailabilities) {
+        $scheduleParser = new ScheduleParser();
 
+        $availabilities = $scheduleParser->createAvailabilitiesForMultipleDay($values, $startingDate);
+
+        $this->assertEquals($expectedAvailabilities, $availabilities);
+    }
+
+    public static function provideValues() : array {
+        return [
+            'test0' => [
+                'values' => [],
+                'startingDate' => Carbon::create(2024),
+                'expectedAvailabilities' => [],
+            ],
+        ];
+    }
 }
