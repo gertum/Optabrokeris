9c253e4a6d871eb6c2cfad2a38db8eff279335b5
Giedrius Tumelis <giedrius@inkodus.lt>
2024-11-07 17:07:38 +0200

    #21: skeletons for xlsx parser unit test.

--- app/Domain/Roster/Hospital/SubjectsXslsParser.php
+++ app/Domain/Roster/Hospital/SubjectsXslsParser.php
@@ -10,6 +10,10 @@ class SubjectsXslsParser
         $result = new SubjectsArray();
         // TODO
 
+        // 1) parse xslx file
+        // 2) find columns 'etatas' and 'darbo valandos'
+        // 3) read row after row, until two empty consequite rows found
+        // 4) read name, position amount, and hours in a day values and create SubjectData element; put it to results array.
         return $result;
     }
 }
