755a26a222c48c05e09493c72028bef3d2ad06f9
Giedrius Tumelis <giedrius@inkodus.lt>
2024-11-07 16:41:38 +0200

    #21: skeletons for xlsx and csv parsers.

---
+++ app/Domain/Roster/Hospital/SubjectsCsvParser.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Domain\Roster\Hospital;
+
+use App\Domain\Roster\SubjectsArray;
+
+class SubjectsCsvParser
+{
+    public function parse(string $csvFile ) : SubjectsArray {
+        $result = new SubjectsArray();
+        // TODO
+
+        return $result;
+    }
+}
---
+++ app/Domain/Roster/Hospital/SubjectsXslsParser.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Domain\Roster\Hospital;
+
+use App\Domain\Roster\SubjectsArray;
+
+class SubjectsXslsParser
+{
+    public function parse(string $xlsxFile ) : SubjectsArray {
+        $result = new SubjectsArray();
+        // TODO
+
+        return $result;
+    }
+}
