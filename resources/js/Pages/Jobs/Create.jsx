8985065e5591f102691f53335a523a17c98e9567
Giedrius Tumelis <giedrius@inkodus.lt>
2024-11-26 15:34:10 +0200

    #31: veikiantis skeletas job sukūrimui.

--- resources/js/Pages/Jobs/Create.jsx
+++ resources/js/Pages/Jobs/Create.jsx
@@ -1,1 +1,33 @@
-TODO
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
+import {Head, router} from '@inertiajs/react';
+import {Button, Col, Divider, Layout, Row, Space, Form, Upload} from 'antd';
+import {useEffect, useState} from 'react';
+const {Content} = Layout;
+
+
+export default function Create({auth}) {
+    return (
+        <AuthenticatedLayout
+            user={auth.user}
+            header={
+                <h2 className="font-semibold text-xl text-gray-800 leading-tight">
+                    {"New job"}
+                </h2>
+            }
+        >
+            <Head title={"New job"}/>
+            <Content
+                style={{
+                    textAlign: 'center',
+                    lineHeight: 4,
+                }}
+            >
+                <Row>
+                    <Col xs={24}>
+                        TODO create job
+                    </Col>
+                </Row>
+            </Content>
+        </AuthenticatedLayout>
+    );
+}
