29293d59b13ee046dfeed4d230aed5a2ff9333ac
Vytenis Ščiukas <vytenis.sciukas@sensus.aero>
2024-12-03 22:59:45 +0200

    Design fixes

--- resources/js/Pages/Jobs/View.jsx
+++ resources/js/Pages/Jobs/View.jsx
@@ -1,26 +1,30 @@
+import React, { useEffect, useState } from 'react';
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
-import {Head} from '@inertiajs/react';
-import {Avatar, Button, Col, Divider, Form, Layout, Row, Space, Upload, Statistic} from 'antd';
-import React, {useEffect, useState} from 'react';
-import {useTranslation} from 'react-i18next';
+import { Head } from '@inertiajs/react';
+import { Button, Col, Divider, Form, Layout, Row, Space, Upload, Statistic, Alert } from 'antd';
+import { useTranslation } from 'react-i18next';
 import axios from 'axios';
-import {useNotification} from "@/Providers/NotificationProvider.jsx";
-import {useConfirmation} from '@/Providers/ConfirmationProvider.jsx';
-import {DownloadOutlined, DislikeOutlined, LikeOutlined, CheckOutlined} from "@ant-design/icons";
+import { useNotification } from '@/Providers/NotificationProvider.jsx';
+import { useConfirmation } from '@/Providers/ConfirmationProvider.jsx';
+import {
+    DownloadOutlined,
+    DislikeOutlined,
+    LikeOutlined,
+    LoadingOutlined,
+    PlayCircleOutlined
+} from '@ant-design/icons';
 
-const {Content} = Layout;
-const {Title} = Head;
+const { Content } = Layout;
+const { Title } = Head;
 
-
-export default function View({auth, job: initialJob}) {
-    const {t} = useTranslation();
-    const {notifySuccess, notifyError} = useNotification();
-    const {requestConfirmation} = useConfirmation();
+export default function JobView({ auth, job: initialJob }) {
+    const { t } = useTranslation();
+    const { notifySuccess, notifyError } = useNotification();
     const [job, setJob] = useState(initialJob);
     const [token, setToken] = useState('');
+    const [countdown, setCountdown] = useState(0);
+    const waitTime = 30;
 
-
-    // TODO use in component this function
     const fetchToken = async () => {
         try {
             const tokenResponse = await axios.get('/login');
@@ -31,7 +35,6 @@ export default function View({auth, job: initialJob}) {
     };
 
     const handleSolve = async () => {
-        // const response = await
         await axios.post(`/api/job/${job.id}/solve?_token=${token}`)
             .catch((error) => {
                 console.log('error response:', error);
@@ -43,22 +46,17 @@ export default function View({auth, job: initialJob}) {
                 notifyError(errorMessage);
             })
             .then((response) => {
-                console.log('solve start response', response);
                 if (response !== undefined) {
-                    notifySuccess(`Solving started`);
-
-                    // be sure the response is a job object
+                    notifySuccess('Solving started');
                     if (response.data.id !== undefined) {
                         setJob(response.data);
-                        reloadAfterSomeTime(31);
+                        setCountdown(waitTime); // Start countdown
                     }
                 }
             });
     };
 
     const handleStop = async () => {
-        // const response =
-
         await axios.post(`/api/job/${job.id}/stop?_token=${token}`)
             .catch((error) => {
                 console.log('error response:', error);
@@ -71,19 +69,17 @@ export default function View({auth, job: initialJob}) {
             })
             .then((response) => {
                 if (response !== undefined) {
-                    notifySuccess(`Solving stop signal sent`);
+                    notifySuccess('Solving stop signal sent');
                     reloadJobContent();
                 }
             });
-
     };
 
     const reloadJobContent = async () => {
         await axios.request({
-                method: 'GET',
-                url: `/api/job/${job.id}?_token=${token}`,
-            }
-        )
+            method: 'GET',
+            url: `/api/job/${job.id}?_token=${token}`,
+        })
             .catch((error) => {
                 console.log('error response:', error);
 
@@ -95,30 +91,31 @@ export default function View({auth, job: initialJob}) {
             })
             .then((response) => {
                 if (response !== undefined) {
-                    // notifySuccess(`Reload job successful`);
                     setJob(response.data);
                 }
             });
-
-    }
+    };
 
     const handleUploadStandard = async () => {
-        // yet we didn't find a way to get upload result, so we make additional request to get job content
         await reloadJobContent();
-    }
-
-    const reloadAfterSomeTime = async (time) => {
-        console.log('Page will be reloaded in ' + time);
-        setTimeout(() => {
-            reloadJobContent();
-            console.log('Page reloaded');
-        }, time * 1000);
-    }
+    };
 
     useEffect(() => {
         fetchToken();
         reloadJobContent();
     }, []);
+
+    // Countdown timer logic
+    useEffect(() => {
+        if (countdown > 0) {
+            const timer = setInterval(() => {
+                setCountdown((prevCountdown) => prevCountdown - 1);
+            }, 1000);
+            return () => clearInterval(timer);
+        } else if (countdown === 0 && job.flag_solving) {
+            handleStop();
+        }
+    }, [countdown, job.flag_solving]);
 
     return (
         <AuthenticatedLayout
@@ -139,82 +136,70 @@ export default function View({auth, job: initialJob}) {
                     lineHeight: 4,
                 }}
             >
-
-                <Row>
-                    <Col xs={24}>
-
-                        <Row align="middle" gutter={16}>
-                            <Col>
-                                <Avatar className="bg-blue-500 text-bold" size={48}>
-                                    {job.type}
-                                </Avatar>
-                            </Col>
-                        </Row>
-                        <Row>
-                            <Col>
-                                <h3>Status</h3>
-                                <Statistic title="Uploaded" value={job.flag_uploaded?'+':'-'} prefix={job.flag_uploaded?<LikeOutlined />:<DislikeOutlined/> } />
-                                <Statistic title="Solving started" value={job.flag_solving?'+':'-'} prefix={job.flag_solving?<LikeOutlined />:<DislikeOutlined/> } />
-                                <Statistic title="Solving DONE" value={job.flag_solved?'+':'-'} prefix={job.flag_solved?<CheckOutlined />:<DislikeOutlined/> } />
-                                <Statistic title="Solver status" value={job.status}  />
-                                <Statistic title="Last error" value={job.error_message}  />
-                            </Col>
-                        </Row>
-                        <Row>
-                            <Col>
-                                <Divider orientation="left">Upload schedule or preferences xlsx file</Divider>
-                                <Form onFinish={() => handleUploadStandard()} className="mt-4">
-                                    <Upload.Dragger
-                                        action={`/api/job/${job.id}/upload?_token=${token}`}
-                                        maxCount={1}
-                                        listType="picture"
-                                        accept=".xlsx"
-                                        onChange={() => handleUploadStandard()}
-                                    >
-                                        {t('step.fileUploadForm.dragFiles')}
-                                        <br/>
-                                        <Space>
-                                            <Button>{t('Upload')}</Button>
-                                        </Space>
-                                    </Upload.Dragger>
-                                </Form>
-
-                            </Col>
-                        </Row>
-
-                        <Row>
-                            <Col>
-                                <Divider orientation="left">Solution</Divider>
-                                <div className="my-2">
-                                    <Space>
-                                        {
-                                            job.status !== 'SOLVING_ACTIVE' ?
-                                                <Button size="large" onClick={handleSolve}>
-                                                    Solve
-                                                </Button>
-                                                :
-                                                <Button size="large" danger onClick={handleStop}>
-                                                    Stop solving
-                                                </Button>
-                                        }
-                                    </Space>
-                                </div>
-
+                <Divider orientation="left">Upload schedule or preferences xlsx file</Divider>
+                <Form onFinish={() => handleUploadStandard()} className="mt-4">
+                    <Upload.Dragger
+                        action={`/api/job/${job.id}/upload?_token=${token}`}
+                        maxCount={1}
+                        listType="picture"
+                        accept=".xlsx"
+                        onChange={() => handleUploadStandard()}
+                    >
+                        {t('step.fileUploadForm.dragFiles')}
+                        <br />
+                        <Space>
+                            <Button>{t('Upload')}</Button>
+                        </Space>
+                    </Upload.Dragger>
+                </Form>
+                <Divider orientation="left">Solution</Divider>
+                {job.error_message && (
+                    <>
+                        <Alert message="Last error" description={job.error_message} type="error" />
+                        <br />
+                    </>
+                )}
+                <Row gutter={16}>
+                    <Col span={12}>
+                        <Statistic
+                            title="Sample"
+                            value={job.flag_uploaded ? 'Uploaded' : 'Not uploaded'}
+                            prefix={job.flag_uploaded ? <LikeOutlined /> : <DislikeOutlined />}
+                        />
+                    </Col>
+                    <Col span={12}>
+                        <Statistic
+                            title="Solution"
+                            value={
+                                job.flag_solving
+                                    ? `${countdown > 0 ? `Ready in ${countdown}s` : 'Not ready'}`
+                                    : 'Ready'
+                            }
+                        />
+                        <Space>
+                            {!!job.flag_solving && (
+                                <Button icon={<LoadingOutlined />} danger onClick={handleStop}>
+                                    Stop solving
+                                </Button>
+                            )}
+                            {!job.flag_solving && (
+                                <Button icon={<PlayCircleOutlined />} onClick={handleSolve}>
+                                    Solve
+                                </Button>
+                            )}
+                            {!job.flag_solving && (
                                 <Button
                                     type="primary"
-                                    shape="round"
-                                    icon={<DownloadOutlined/>}
-                                    size="large"
+                                    icon={<DownloadOutlined />}
                                     href={`/api/job/${job.id}/download?_token=${token}`}
                                     target="_blank"
                                 >
                                     Download
                                 </Button>
-                            </Col>
-                        </Row>
+                            )}
+                        </Space>
                     </Col>
                 </Row>
-                <Divider/>
             </Content>
         </AuthenticatedLayout>
     );
